85a165188af98f3c5bc5393cdabaef6fa3d30c3f
muhammadasadullah-boop <muhammadasadullah@ontariotechu.net>
2025-11-05 23:46:04 -0500

    Beggining project stage(first commit)

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Past Projects' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
+module.exports = router;
